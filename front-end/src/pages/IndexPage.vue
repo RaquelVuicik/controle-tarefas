68de081097d160f746ffb5f2e9a24ca1936d093a
Raquel Vuicik <raquel.vuicik@artemidas.net>
2024-12-13 17:52:20 -0300

    axios

--- front-end/src/pages/IndexPage.vue
+++ front-end/src/pages/IndexPage.vue
@@ -5,6 +5,8 @@
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
+    <button @click="listarUsuarios">Listar</button>
+    <pre>{{ usuarios }}</pre>
   </q-page>
 </template>
 
@@ -12,6 +14,21 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      usuarios: []
+    }
+  },
+  mounted () {
+    this.listarUsuarios()
+  },
+  methods: {
+    listarUsuarios () {
+      this.$api.get('/user').then((response) => {
+        this.usuarios = response.data
+      })
+    }
+  }
 });
 </script>
